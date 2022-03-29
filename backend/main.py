eaf4556c3dac9db0711bf204039a7d1b6d26527e
cn-eveer <sienreeve46@gmail.com>
2022-03-30 04:48:18 +0900

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,58 @@
-from flask import Flask, jsonify, json, request
-import os;
+from flask import Flask, jsonify, request
+import os
 import librosa
 import librosa.display
 import numpy as np
 from tensorflow import keras
-from tensorflow.keras.utils import to_categorical 
 
-model = keras.models.load_model('C:\\Users\\khale\\Documents\\GitHub\\mimi4me\\backend\\')
-path = 'save.mp4'
+import warnings
+warnings.filterwarnings('ignore')
 
-causes = ['AC', 'Carn Horn', 'Kids Playing', 'Dog Bark', 'Drilling', 'Engine Idling', 'Gun Shot', 'Jackhammer', 'Siren', 'Street Music']
-decibels = ""
-response = ""
+MODEL = keras.models.load_model("./")
+PATH = 'save.mp4'
+
+CAUSES = ['AC', 'Carn Horn', 'Kids Playing', 'Dog Bark', 'Drilling',
+          'Engine Idling', 'Gun Shot', 'Jackhammer', 'Silent', 'Street Music']
+DECIBELS = 0
+CAUSE = ""
 
 def process():
-    global decibels
-    X, sample_rate = librosa.load(path, res_type='kaiser_fast')
-    decibels = f'{np.average(librosa.amplitude_to_db(X))}'
-    return np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T,axis=0).reshape((1,16,8,1))     
+    
+    global DECIBELS,CAUSES,CAUSE,PATH
 
+    x, sample_rate = librosa.load(PATH, res_type='kaiser_fast')
+    second = []
+    for s in range(0,len(x),sample_rate):
+        second.append( np.abs(x[s:s+sample_rate]).mean())   
+    noise = np.average(librosa.amplitude_to_db(x))
+    data = np.mean(librosa.feature.melspectrogram(
+        y=x, sr=sample_rate).T, axis=0
+    ).reshape((1, 16, 8, 1))
 
+    noise += 100
+    DECIBELS = int(noise)
+
+    prediction = np.argmax(MODEL.predict(data))
+    CAUSE = f'{CAUSES[prediction]}'
+    print(DECIBELS,CAUSE)
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def respond():
-    global response
-    global causes
+
+    global DECIBELS,CAUSES,CAUSE,PATH
+
     if(request.method == 'POST'):
-        #getting and saving file
         f = request.files.get('audio')
-        f.save(path)
-        #processing file
-        data = process()
-        prediction = np.argmax(model.predict(data))
-        response = f'{causes[prediction]}'
-        os.remove(path)
-        return " "
+        f.save(PATH)
+        process()
+        os.remove(PATH)
+        return ""
     else:
-        return jsonify({'cause' : response, "decibels" : decibels})
+        return jsonify({'cause': CAUSE, "decibels": DECIBELS})
+
 
 if __name__ == '__main__':
     app.run()
